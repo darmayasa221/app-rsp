b300d83cbe2278a15ea375f37eba45f526bccbd8
darmayasa221 <darmayasa221@gmail.com>
2020-12-21 18:33:53 +0800

    test

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         test
         </p>
-          Learn React
+    
       </header>
     </div>
   );
